51eb025a13bff96813ebebea91b29db681ad0f4d
Angeltamgho87 <94420069+Angeltamgho87@users.noreply.github.com>
2022-07-26 05:02:18 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+const list = document.querySelectorAll(".list");
+
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+
+list.forEach((item) => item.addEventListener("click", activeLink));
